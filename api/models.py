9ecfda25b870ce4dea8d2c066765976133b85f82
LNkholise <leonkholise@gmail.com>
2025-01-04 21:21:39 +0200

    Made a template model for products

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Product(models.Model):
+	name = models.CharField(max_length=255)
+	manufacturer = models.CharField(max_length=255)
+	price = models.DecimalField(max_digits=10, decimal_places=2)
+	quantity = models.PositiveIntegerField(validators = [MinValueValidator(0), MaxValueValidator(1)],)
+	description = models.TextField(max_length=1000)
+	image = models.ImageField(upload_to='product_images/', null=False, blank=False)
+	
+	def __str__(self):
+		return self.name
+    
