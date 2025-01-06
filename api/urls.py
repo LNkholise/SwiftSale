25d9b69fc35717fa761655d885ea9d49abf48828
LNkholise <leonkholise@gmail.com>
2025-01-06 23:06:43 +0200

    Updated urls.py to handle buy requests

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductViewSet
+from .views import ProductViewSet, buy_request
 
 urlpatterns = [
     path('products/', ProductViewSet.as_view({'get':'list','post': 'create'}), name='product-list'),
+    path('buy/', buy_request, name='buy-request'),
 ]
 
